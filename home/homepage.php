f7cf176adadb337a94bca54d222fd005535dea67
Russo Salvatore <russo.salvatore@osdbsesto>
2022-01-14 16:13:14 +0100

    1 test immage failed

--- home/homepage.php
+++ home/homepage.php
@@ -7,8 +7,10 @@
 <?php
 echo"<form align='center' heigth='500px' method='POST'>
 </br>
-inserisci il tuo nickname
-<input type='text'  name='nome'></br>
-
+inserisci il tuo nickname <input type='text'  name='nick'></br>
+</br>
+inserisci la tua password <input type='text' name='pass'></br>
+</br>
+<input type='submit'>
 </form>";
 ?>
