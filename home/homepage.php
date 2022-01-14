1fd82ce61e4b41b66559f6cb4ef9267054e8346e
Russo Salvatore <russo.salvatore@osdbsesto>
2022-01-14 15:29:33 +0100

    creazion first part

---
+++ home/homepage.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+</html>
+
+<?php
+echo"<form align='center' heigth='500px' method='POST'>
+</br>
+inserisci il tuo nickname
+<input type='text'  name='nome'></br>
+
+</form>";
+?>
