40490171162ec274371c2aeb2b8acf93c81edbc9
Russo Salvatore <russo.salvatore@osdbsesto>
2022-01-19 11:12:14 +0100

    control posizion homepage

--- home/homepage.php
+++ home/homepage.php
@@ -6,11 +6,13 @@
 
 <?php
 echo"<form align='center' heigth='500px' method='POST'>
+<div>
 </br>
 inserisci il tuo nickname <input type='text'  name='nick'></br>
 </br>
 inserisci la tua password <input type='text' name='pass'></br>
 </br>
-<input type='submit'>
+<input type='submit' value='accedi'>
+</div>
 </form>";
 ?>
