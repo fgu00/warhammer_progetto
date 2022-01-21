16fe4f8dd38dcd82ed987c362b7762236d31a920
Russo Salvatore <russo.salvatore@osdbsesto>
2022-01-21 16:13:06 +0100

    update nuovo_utente part 2

--- home/nuovo_utente.php
+++ home/nuovo_utente.php
@@ -11,5 +11,17 @@ echo "<form action='homepage.php'>
 <input type='submit'value='home'>
 </form>";
 
-
+echo"<form align='center' method='POST'>
+<font color='white'size='4'>inserisci il nome <input type='text'  name='nome'></font></br>
+<font color='white'size='4'>inserisci il  cognome <input type='text'  name='cnome'></font></br>
+<font color='white'size='4'>inserisci l'indirizzo email <input type='text'  name='email'></font></br>
+<font color='white'size='4'>inserisci il nickname <input type='text'  name='nick'></font></br>
+<font color='white'size='4'>inserisci la password <input type='text'  name='pass'></font></br>
+<font color='white'size='4'>conferma la password <input type='text'  name='pass2'></font></br>
+</form>";
+if($_POST['pass']==$_POST['pass2']){
+echo "afo";
+}else{
+echo "le password non coincidono";
+}
 ?>
