396e571d65841977246f0ab127efa5a84de90318
Russo Salvatore <russo.salvatore@osdbsesto>
2022-01-21 15:55:34 +0100

    update nuovo_utente + utilizzare bootstrap

--- home/nuovo_utente.php
+++ home/nuovo_utente.php
@@ -1,3 +1,15 @@
+<html>
+    <head>
+    <link href="utente.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+    </body>
+</html>
+
 <?php
-echo"work in progress"
+echo "<form action='homepage.php'>
+<input type='submit'value='home'>
+</form>";
+
+
 ?>
